b47044d1cdd35a992c9029911872aee82e1f4b93
Manish Pandey <manishpandey012345@gmail.com>
2025-11-11 16:49:22 +0530

    Added new endpoints for updating and deleting employees

--- src/main/java/org/learnspring/curddemo/CurddemoApplication.java
+++ src/main/java/org/learnspring/curddemo/CurddemoApplication.java
@@ -19,8 +19,54 @@ public class CurddemoApplication {
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
         return runner -> {
-           createStudent(studentDAO);
+           //createMultipleStudent(studentDAO);
+
+            readStudent(studentDAO);
         };
+    }
+
+    private void readStudent(StudentDAO studentDAO) {
+
+        // create a student object
+        System.out.println("Creating new student object..");
+        Student tempStudent =new Student("manish", "kumar", "manishkumar@gmail.com");
+
+        // save the student object
+
+        System.out.println("saving the student  ");
+        studentDAO.save(tempStudent);
+
+        // display the id of the saved student
+
+        int theId= tempStudent.getId();
+        System.out.println("saved student. Generated id: " + theId);
+
+        //retrieve student based on the id: primary key
+
+        System.out.println("Retrieving student with id: " + theId);
+        Student myStudent= studentDAO.findById(theId);
+
+        // display the student
+        System.out.println("Found the student: " + myStudent);
+
+    }
+
+    private void createMultipleStudent(StudentDAO studentDAO) {
+        // create multiple student objects
+        System.out.println("creating new student object...");
+        Student tempStudent1 = new Student("a", "b", "ab@example.com");
+        Student tempStudent2 = new Student("c", "d", "cd@example.com");
+
+        Student tempStudent3 = new Student("e", "f", "ef@example.com");
+
+
+        // save the student objects
+
+
+        System.out.println("saving the student");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
     }
 
     private void createStudent(StudentDAO studentDAO) {
